104afbd2c7a8af0cbf156899d55a5b8cfadf70be
Dylan Rodrigues <78458509+D1Rodrigues@users.noreply.github.com>
2022-03-13 18:37:39 -0400

    Add files via upload

---
+++ dropdown-delete.php
@@ -0,0 +1,27 @@
+<?php
+    require_once "includes/db.inc.php";
+    require_once "includes/delete.inc.php";
+
+    $table = $_POST["TName"];
+    $columns = $_POST["Columns"];
+    $conditions = $_POST["Conditions"];
+
+    $m = new Deletion("$table");
+    if(!empty($conditions) && !ctype_space($email))
+    {
+        $res = $m->deleteRecords($conditions);
+    }
+    else{
+        $res = $m->deleteRecords();
+    }
+
+
+    if ($res)
+    {
+        header("Location: P_Home.php");
+    }
+    else
+    {
+        echo "Error in the query you entered.";
+    }
+?>
