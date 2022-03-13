81babb43168fc64a21e8a7f7b14c05ac5c446eca
Dylan Rodrigues <78458509+D1Rodrigues@users.noreply.github.com>
2022-03-12 22:06:10 -0500

    Update insert.inc.php

--- includes/insert.inc.php
+++ includes/insert.inc.php
@@ -13,7 +13,6 @@
         $sql = "INSERT INTO " . $this->table . "(" . $fields . ") VALUES
         (" . $values . ")";
 
-        //$sql = "SELECT id, firstname, lastname FROM StRec";
         $result = $conn->query($sql);
         
         return $result;
