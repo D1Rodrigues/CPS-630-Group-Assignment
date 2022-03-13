875de1181f34702e2020c9889377f87544abb9fd
Dylan Rodrigues <78458509+D1Rodrigues@users.noreply.github.com>
2022-03-13 19:38:09 -0400

    Update delete.inc.php

--- includes/delete.inc.php
+++ includes/delete.inc.php
@@ -7,9 +7,10 @@
           parent::__construct($tbl);
       }
 
-      public function deleteRecords($conditionals = "none")
+      public function deleteRecords($conditionals="none")
       {
         $conn = $this->connect();
+        
         if(strcmp($conditionals, "none") !== 0)
         {
             $sql = "DELETE FROM " . $this->table . " WHERE " . $conditionals;
@@ -18,14 +19,10 @@
         {
             $sql = "DELETE FROM " . $this->table;
         }
- 
 
-        //$sql = "SELECT id, firstname, lastname FROM StRec";
         $result = $conn->query($sql);
         
         return $result;
-
-        //$conn->close();
 
       }
     }
